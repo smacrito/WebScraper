3e47e6ad5759e027b837aaf54f35cf1df849c5d2
Sammy M <smacrito@gmail.com>
2020-04-24 15:13:43 -0700

    Added comments, clarified variables

--- scraper.py
+++ scraper.py
@@ -1,35 +1,27 @@
 from selenium import webdriver as wd
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import csv
 
 def main():
-    
-    
-    driver = wd.Firefox(executable_path=r'C:\Users\Sammy\Desktop\WebScraper\geckodriver.exe')
+                                        #CHANGE TO YOUR DRIVER PATH -- GECKO DRIVER IS MOZILLA
+    driver = wd.Firefox(executable_path=r'C:\Users\Sammy\Desktop\CovidDataScraper\WebScraper\geckodriver.exe')
     driver.set_page_load_timeout(30)
     driver.get('https://www.dph.illinois.gov/covid19/covid19-statistics')
+    
     #all_element = driver.findElement(By.linkText("All").click()
-    all_element = driver.find_element_by_xpath('//html//body//div[1]//div[4]//div//article//div//div//div//ul[3]//li[42]//a').click()
+    all_element = driver.find_element_by_xpath('//html//body//div[1]//div[4]//div//article//div//div//div//ul[3]//li[43]//a').click()
+
     table_element = driver.find_element_by_id('detailedData')
     table_text=table_element.text
     print(table_text)
-    #print(all_element)
-
-
-
+    
+    #write to csv (may take some time)
     with open('data.csv','w',newline='') as csvfile:
-              wr = csv.writer(csvfile)
-              #headers
-               
-              for row in table_element.find_elements_by_css_selector('tr'):
-                      wr.writerow([d.text for d in row.find_elements_by_css_selector('td')])
 
+        wr = csv.writer(csvfile)
+        #find table
+        for row in table_element.find_elements_by_css_selector('tr'):
+            #find rows
+            wr.writerow([data.text for data in row.find_elements_by_css_selector('td')])
     
 if __name__ == "__main__":
     main()
